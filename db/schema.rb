16bead4ec673c6c4e9081f31fe54a7e532863a39
Angeline Chew <chewmunyoke@users.noreply.github.com>
2018-03-18 23:12:34 +0800

    PostgreSQL DB configuration

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180312175408) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "title", default: "Untitled"
     t.boolean "completed", default: false
